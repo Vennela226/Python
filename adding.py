2192e895e206c0a70343db2c6d663316b7d07bd7
VENNELA2004 <veluruvennela07@gmail.com>
2026-01-07 12:43:37 +0530

    Add files via upload

---
+++ adding.py
@@ -0,0 +1,8 @@
+books={
+    "Harry Potter":1997,"The Hobbit":1937,"Atomic Habits":2018
+}
+books["Rich Dad poor Dad"]=1997
+books["The Hobbit Revised"]=books.pop("The Hobbit")
+year=books["Atomic Habits"]
+print(books)
+print("Year of Atomic Habits:",year)
